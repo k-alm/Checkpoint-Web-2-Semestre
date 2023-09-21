76e29829be49b4834b20749dca54519c45e692f4
k-alm <kauan.alm.almeida@gmail.com>
2023-09-21 20:17:33 -0300

    Cabeçalho feito

---
+++ src/components/Cabecalho.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import { AiFillHome as H } from "react-icons/ai";
+
+export default function Cabecalho() {
+    return (
+        <>
+            <header>
+                <h1>Vite + React / Coded By - PF0670</h1>
+                <h2>ISOLANTE - LINDO</h2>
+                <ul>
+                    <li>
+                        <Link to="/">HOME - <H /></Link>
+                    </li>
+                    <li>
+                        <Link to="/produtos">PRODUTOS</Link>
+                    </li>
+                </ul>
+            </header>
+        </>
+    );
+}
