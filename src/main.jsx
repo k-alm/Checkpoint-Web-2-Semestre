d66ad3a2b5239a51f18ce139a2e91db84b6e10d6
Scaffx <Scaff.scaff444@gmail.com>
2023-09-21 21:06:08 -0300

    Main.jsx criada

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Home from './routes/Home.jsx'
+import Produtos from './routes/Produtos.jsx'
+import EditarProdutos from './routes/EditarProdutos.jsx'
+import ExcluirProduto from './routes/ExcluirProduto.jsx'
+import Error404 from './routes/Error404.jsx'
+
+const router = createBrowserRouter([
+
+  {path: '/', element: <App/>,
+    errorElement: <Error404/>,
+    children:[
+      {path: '/', element: <Home/>}, 
+      {path: '/produtos', element: <Produtos/>}, 
+      {path: '/editar/produtos/:id', element: <EditarProdutos/>},
+      {path: '/excluir/produtos/:id', element: <ExcluirProduto/>}
+
+    ]
+}
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+
+  <RouterProvider router={router}/>
+
   </React.StrictMode>,
 )
