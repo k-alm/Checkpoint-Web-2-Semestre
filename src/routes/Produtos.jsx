1ce8744353b487e76a4632d25692360adc8dffe9
nichol6s <nicholas.jake15@gmail.com>
2023-09-21 21:59:39 -0300

    Removendo o counter 1

--- src/routes/Produtos.jsx
+++ src/routes/Produtos.jsx
@@ -32,9 +32,9 @@ export default function Produtos() {
     return (
         <div>
             <h1>LISTA DE PRODUTOS</h1>
-            <div>
+            {/* <div>
                 <button onClick={() => setCounter(counter + 1)}>COUNTER - {counter}</button>
-            </div>
+            </div> */}
             <div>
                 <button onClick={() => setCounter2(counter2 + 1)}>COUNTER2 - {counter2}</button>
             </div>
