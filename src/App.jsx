c7e642b606b8c8d0dae0f9aee7c5c5252b96857d
Scaffx <Scaff.scaff444@gmail.com>
2023-09-21 21:02:29 -0300

    Iniciando APP.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
-import './App.css'
-
-function App() {
-
-  return (
-    <>
-      
-    </>
-  )
-}
-
-export default App
+//Importando dependências e ou arquivos que vamos utilizar e ou referenciar.
+import Cabecalho from "./components/Cabecalho"
+//import Section from "./components/Section"
+import Footer from "./components/Footer"
+import { Outlet } from "react-router-dom";
+//import { useState } from "react";
