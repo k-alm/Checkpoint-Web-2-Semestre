139db3872567d2548e24516622da11f62ab8754a
nichol6s <nicholas.jake15@gmail.com>
2023-10-13 16:12:37 -0300

    Atualizando o repositorio para a versao mais recente dos arquivos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import Cabecalho from "./components/Cabecalho"
+import Cabecalho from "./components/Cabecalho/Cabecalho"
 import { Outlet } from "react-router-dom";
 import Rodape from "./components/Rodape";
+import styles from "./App.module.css"
 
 export default function App(){
 
   return (
     <>
-      <div>
+      <div className={styles.container} >
         <Cabecalho/>
         <Outlet/>
         <Rodape/>
